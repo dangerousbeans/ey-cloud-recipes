fc0d76380c16f55896bdc26890bb68eb13b39afc
Scott M. Likens <slikens@engineyard.com>
2010-08-29 14:54:24 -0700

    Last time I said it works? I was kidding.  Try this.

--- cookbooks/couchdb/recipes/default.rb
+++ cookbooks/couchdb/recipes/default.rb
@@ -3,51 +3,54 @@
 # Recipe:: default
 #
 
-package "dev-db/couchdb" do
-  version "0.9.1"
-  action :purge
-end
+if ['solo','app_master','app','util'].include?(node[:instance_role])
 
-execute "remove_old_default_ini" do
-  command "rm /etc/couchdb/default.ini"
-  only_if { "cat /etc/couchdb/default.ini | grep 0.9.1" }
-end
+  package "dev-db/couchdb" do
+    version "0.9.1"
+    action :purge
+  end
 
-require_recipe "couchdb::compile_from_source"
+  execute "remove_old_default_ini" do
+    command "rm /etc/couchdb/default.ini"
+    only_if { "cat /etc/couchdb/default.ini | grep 0.9.1" }
+  end
 
-storage_dir = case node[:instance_role]
-  when "db_master", "db_slave", "solo" then "/db"
-  else "/data"
-end
+  require_recipe "couchdb::compile_from_source"
 
-directory "#{storage_dir}/couchdb" do
-  owner "couchdb"
-  group "couchdb"
-  mode 0750
-end
+  storage_dir = case node[:instance_role]
+    when "db_master", "db_slave", "solo" then "/db"
+    else "/data"
+  end
 
-template "/etc/couchdb/local.ini" do
-  source "local.ini.erb"
-  mode 0640
-  owner "couchdb"
-  group "couchdb"
-  variables(
-    :bind => "0.0.0.0",
-    :port => 5984,
-    :database_dir   => "#{storage_dir}/couchdb",
-    :view_index_dir => "#{storage_dir}/couchdb"
-  )
-  backup false
-end
+  directory "#{storage_dir}/couchdb" do
+    owner "couchdb"
+    group "couchdb"
+    mode 0750
+  end
 
-execute "chown_etc_couchdb" do
-  command "chown -R couchdb:couchdb /etc/couchdb"
-  action :run
-end
+  template "/etc/couchdb/local.ini" do
+    source "local.ini.erb"
+    mode 0640
+    owner "couchdb"
+    group "couchdb"
+    variables(
+      :bind => "0.0.0.0",
+      :port => 5984,
+      :database_dir   => "#{storage_dir}/couchdb",
+      :view_index_dir => "#{storage_dir}/couchdb"
+    )
+    backup false
+  end
 
-execute "start CouchDB" do
-  command %Q{ 
-    echo "/etc/init.d/couchdb start &> /dev/null" | at now
-  }
-  not_if "/etc/init.d/couchdb status"
+  execute "chown_etc_couchdb" do
+    command "chown -R couchdb:couchdb /etc/couchdb"
+    action :run
+  end
+
+  execute "start CouchDB" do
+    command %Q{ 
+      echo "/etc/init.d/couchdb start &> /dev/null" | at now
+    }
+    not_if "/etc/init.d/couchdb status"
+  end
 end
