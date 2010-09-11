7d6d4b526b8f57561200b548104dc77e7da89686
Scott M. Likens <slikens@engineyard.com>
2010-09-10 21:00:32 -0700

    - Polishing up the 8.4.2 bits...
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/postgres/recipes/server_install.rb
+++ cookbooks/postgres/recipes/server_install.rb
@@ -50,4 +50,52 @@ when "9.0"
   execute "eselect postgresql set #{postgres_version}" do
   action :run
   end
+
+when "8.4"
+
+    execute "emerge -C =dev-db/postgresql-server-8.3.5" do
+    action :run
+    only_if { FileTest.directory?("/var/db/pkg/dev-db/postgresql-base-8.3.5") }
+  end
+
+  execute "emerge -C =dev-db/postgresql-base-8.3.5" do
+    action :run
+    only_if { FileTest.directory?("/var/db/pkg/dev-db/postgresql-server-8.3.5") }
+  end
+
+  directory "/engineyard/portage/packages" do
+    action :create
+    mode 0755
+    owner "root"
+    group "root"
+  end
+
+  directory "/engineyard/portage/packages/dev-db" do
+    action :create
+    mode 0755
+    owner "root"
+    group "root"
+  end
+
+  packages_to_install = ['dev-db/postgresql-base-8.4.2.tbz2','dev-db/postgresql-server-8.4.2.tbz2'].each do |packages_to_fetch|
+    remote_file "/engineyard/portage/packages/#{packages_to_fetch}" do
+      source "http://ey-portage.s3.amazonaws.com/#{node[:kernel][:machine]}/#{packages_to_fetch}"
+      not_if { FileTest.exists?("/engineyard/portage/packages/#{packages_to_fetch}") }
+      backup false
+    end
+  end
+
+  execute "emerge /engineyard/portage/packages/dev-db/postgresql-base-8.4.2.tbz2" do
+    action :run
+    not_if { FileTest.directory?("/var/db/pkg/dev-db/postgresql-base-8.4.2") }
+  end
+
+  execute "emerge /engineyard/portage/packages/dev-db/postgresql-server-8.4.2.tbz2" do
+    action :run
+    not_if { FileTest.directory?("/var/db/pkg/dev-db/postgresql-server-8.4.2") }
+  end
+
+  execute "eselect postgresql set #{postgres_version}" do
+    action :run
+  end
 end
