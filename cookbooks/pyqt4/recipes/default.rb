c928a062e7c149e122e40261d4e9c073acd2084a
Scott M. Likens <slikens@engineyard.com>
2010-11-09 15:46:19 -0800

    add pyqt4 base recipe

---
+++ cookbooks/pyqt4/recipes/default.rb
@@ -0,0 +1,48 @@
+enable_package "x11-libs/qt-core" do 
+version "4.4.2" 
+end
+
+enable_package "x11-libs/qt-qt3support" do 
+version "4.4.2" 
+end
+
+enable_package "x11-libs/qt-sgl" do 
+version "4.4.2" 
+end
+
+enable_package "x11-libs/qt-webkit" do 
+version "4.4.2" 
+end
+
+enable_package "x11-libs/qt-gui-4.4.2" do 
+version "4.4.2" 
+end
+
+enable_package "x11-libs/qt-script" do 
+version "4.4.2" 
+end
+
+enable_package "dev-python/PyQT4" do 
+version "4.4.3" 
+end
+
+enable_package "dev-python/sip-4.7.7" do 
+version "4.4.3" 
+end
+
+# these files are here because I could not get this file with the proxy enabled
+
+remote_file "/engineyard/portage/distfiles/sip-4.7.7.tar.gz" do 
+source "http://service-spi.web.cern.ch/service-spi/external/tarFiles/sip-4.7.7.tar.gz"; 
+backup 0 
+end
+
+remote_file "/engineyard/portage/distfiles/PyQt-x11-gpl-4.4.3.tar.gz" do 
+source "http://ancient-distfiles.s3.amazonaws.com/PyQt-x11-gpl-4.4.3.tar.gz"; 
+backup 0 
+end
+
+package "dev-python/PyQT4" do 
+version "4.4.3" 
+action :install 
+end
