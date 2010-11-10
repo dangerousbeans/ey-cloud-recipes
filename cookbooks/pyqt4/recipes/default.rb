98c80f9a8c7403ed571f9ffd864d43cf2db66566
Scott M. Likens <slikens@engineyard.com>
2010-11-09 17:24:13 -0800

    - Tiny updates
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/pyqt4/recipes/default.rb
+++ cookbooks/pyqt4/recipes/default.rb
@@ -43,18 +43,24 @@ version "4.7.7"
 end
 
 # these files are here because I could not get this file with the proxy enabled
+# not required already updated on the proxy :)
+#remote_file "/engineyard/portage/distfiles/sip-4.7.7.tar.gz" do 
+#source "http://service-spi.web.cern.ch/service-spi/external/tarFiles/sip-4.7.7.tar.gz"; 
+#backup 0 
+#end
 
-remote_file "/engineyard/portage/distfiles/sip-4.7.7.tar.gz" do 
-source "http://service-spi.web.cern.ch/service-spi/external/tarFiles/sip-4.7.7.tar.gz"; 
-backup 0 
-end
-
-remote_file "/engineyard/portage/distfiles/PyQt-x11-gpl-4.4.3.tar.gz" do 
-source "http://ancient-distfiles.s3.amazonaws.com/PyQt-x11-gpl-4.4.3.tar.gz"; 
-backup 0 
-end
+#remote_file "/engineyard/portage/distfiles/PyQt-x11-gpl-4.4.3.tar.gz" do 
+#source "http://ancient-distfiles.s3.amazonaws.com/PyQt-x11-gpl-4.4.3.tar.gz"; 
+#backup 0 
+#end
 
 package "dev-python/PyQt4" do 
 version "4.4.3" 
 action :install 
+end
+
+package "x11-libs/qt-webkit" do
+  version "4.4.2"
+  action :install
+end
 end
