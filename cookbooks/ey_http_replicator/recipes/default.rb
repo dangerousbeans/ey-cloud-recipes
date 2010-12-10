ba2e5e15e475940b62177b2208594ada9ba1ad69
Scott M. Likens <slikens@engineyard.com>
2010-12-10 15:04:57 -0800

    - Update as per Ticket ZD#19205
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/ey_http_replicator/recipes/default.rb
+++ cookbooks/ey_http_replicator/recipes/default.rb
@@ -1,5 +1,10 @@
+execute "verify_binhost" do
+  command "grep -q -v "PORTAGE_BINHOST.*http://ec2-174-129-241-164\.compute-1\.amazonaws.com"/etc/make.conf"
+end
+
 update_file "add http_proxy to /etc/make.conf" do
     path "/etc/make.conf"
     body "http_proxy=\"http://cloud.replicator.eydistro.engineyard.com:5254\""
+    not_if { "grep http_proxy /etc/make.conf"}
 end
 
